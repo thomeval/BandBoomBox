ec29dae1a5bd02f56a115ef2b7121d691bb20af9
Thome Valentin <thomeval@hotmail.com>
2023-10-31 16:01:04 +0200

    Removed some unused Unity packages Removed Nuget for Unity, switched to static DLL instead for Json.NET

--- Assets/Scripts/ChartEditor/ChartEditorMenuManager.cs
+++ Assets/Scripts/ChartEditor/ChartEditorMenuManager.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Linq;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
--- Assets/Scripts/Input/InputManager.cs
+++ Assets/Scripts/Input/InputManager.cs
@@ -1,8 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
-using Assets.Scripts.Misc;
 using System.Collections;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
